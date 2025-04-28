6de42530d692122d660f3d6d54ab560c74e26a31
Davi Santos <daviir17@gmail.com>
2025-04-27 22:28:09 -0300

    organização das pastas, css modules, variaveis de css e componente Heading

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/theme.css';
+import './styles/global.css';
+import Heading from './components/Heading';
+
+function App() {
+
+  return (
+    <>
+      <Heading/>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio laborum praesentium ut! Saepe ab praesentium unde culpa quasi consequatur nulla illum molestias incidunt aspernatur amet perferendis, odio ad explicabo hic.</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.css';
+
+export default function Heading() {
+  console.log(styles)
+  return <div className={styles.heading}>headig</div>;
+}
